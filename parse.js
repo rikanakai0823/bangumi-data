4380f66e815e3abcb40ed7357c9aa7eddde79caa
rikanakai0823 <48682370+rikanakai0823@users.noreply.github.com>
2019-03-18 23:01:59 +0900

    fix cid and pid rules

--- parse.js
+++ parse.js
@@ -4,7 +4,8 @@ var config = require('./config')
 var schedule = fse.readJsonSync(process.argv[2])
 
 schedule.forEach(function(chan) {
+  if (config.cids.indexOf(chan.id) == -1) return 0
   chan.programs.forEach(function(prog) {
-    fse.outputJsonSync(`./programs/${prog.id.slice(-2)}/${prog.id}.json`, prog)
+    fse.outputJsonSync(`./programs/${parseInt(prog.id, 36).toString(16).slice(-2)}/${prog.id}.json`, prog)
   })
 })
