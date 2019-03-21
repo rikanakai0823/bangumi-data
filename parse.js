b3f61d0d2b2f46e356372b655f8f0b76f378cb27
rikanakai0823 <48682370+rikanakai0823@users.noreply.github.com>
2019-03-21 23:34:28 +0900

    resolve id conflict

--- parse.js
+++ parse.js
@@ -6,6 +6,9 @@ var schedule = fse.readJsonSync(process.argv[2])
 schedule.forEach(function(chan) {
   if (config.cids.indexOf(chan.id) == -1) return 0
   chan.programs.forEach(function(prog) {
-    fse.outputJsonSync(`./programs/${parseInt(prog.id, 36).toString(16).slice(-2)}/${prog.id}.json`, prog)
+    let stime = new Date(prog.start)
+    let mon = `0${stime.getMonth() + 1}`.slice(-2)
+    let date = `0${stime.getDate()}`.slice(-2)
+    fse.outputJsonSync(`./programs/${stime.getFullYear()}/${mon}/${date}/${prog.id}.json`, prog)
   })
 })
